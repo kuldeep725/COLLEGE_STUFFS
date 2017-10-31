9e5f579f49ba64b408aad1089d30ed42a02be622
kuldeep725 <kunal725725@gmail.com>
2017-11-01 01:39:09 +0530

    j

---
+++ CSlAB_2017/DFS/dfs.c
@@ -0,0 +1,29 @@
+/*
+
+    * Name      : Kuldeep Singh Bhandaridfs
+    * Roll No.  : 111601009
+
+*/
+#include "stack.h"
+#include "graph.h"
+
+int main () {
+
+	int root;
+
+	GRAPH *graph = (GRAPH *) malloc(sizeof(GRAPH));
+	graph = readAdjacentMatrix (graph);
+
+	if (graph == NULL) {
+		
+		printf("ERROR... RETURNED 1\n");			//if file is not opening
+		return 1;
+
+	}
+
+	printf("ENTER THE STARTING VERTEX : \n");
+	scanf ("%d", &root);
+
+	return 0;
+
+}
