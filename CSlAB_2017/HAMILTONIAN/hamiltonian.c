940b2cf7e529681a74793841f4cbc4198ced4f34
kuldeepsinghbhandari <kunal725725@gmail.com>
2017-10-31 19:27:14 +0530

    k

--- CSlAB_2017/HAMILTONIAN/hamiltonian.c
+++ CSlAB_2017/HAMILTONIAN/hamiltonian.c
@@ -8,12 +8,35 @@
 #include "queue.h"
 #include "graph.h"
 
+void findHamiltonianPath (GRAPH *graph) {
+    int i, j;
+    Queue *queue = createQueue ();
+    int visited[32] = {0};
+    int start = 0;
+    deQueue (queue, start);
+
+    while (!IsQueueEmpty(queue)) {
+        int currentNode = deQueue (queue);
+
+        if (currentNode != start) visited[currentNode] = 1;
+
+        for (i = 0; i < graph->n; i++) {
+            if (a[currentNode][i] == 1) {
+                if (!visited[i]) {
+
+                }
+            }
+        }
+    }
+
+}
+
 int main () {
 
     GRAPH *graph;
 
-    printf("Enter filename : \n");
-    scanf("%s", (graph->fileName));         	//asking user for input file name
+    // printf("Enter filename : \n");
+    // scanf("%s", (graph->fileName));         	//asking user for input file name
 
     graph = readAdjacentMatrix(graph);					//reading adjacent matrix
     findHamiltonianPath (graph);
